a4872f8859e0f20f85482e406de655974b852d29
PythonCakeMing <1851464029@qq.com>
2016-07-28 10:10:21 +0800

    add controller

---
+++ app/Http/Controllers/ThyroidClass/PhaseController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\ThyroidClass;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PhaseController extends Controller
+{
+    //
+}
