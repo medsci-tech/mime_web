8ba0db06d0acffd21e73cd16f5e28ccb8cb0a218
余鹏飞 <501254061@qq.com>
2016-08-02 12:50:01 +0800

    页面调整

--- resources/views/thyroid-class/index.blade.php
+++ resources/views/thyroid-class/index.blade.php
@@ -1,7 +1,257 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: pytho
- * Date: 2016/7/28
- * Time: 13:55
- */
+@extends('layouts.app')
+
+@section('title','公开课首页')
+
+@section('page_id','open_course')
+
+@section('css')
+  <link rel="stylesheet" href="/vendor/foundation-6.2.3-complete/css/foundation.min.css">
+  <link rel="stylesheet" href="/vendor/font-awesome-4.6.2/css/font-awesome.min.css">
+  <link rel="stylesheet" href="/vendor/swiper/swiper-3.3.0.min.css">
+  <style>
+    .swiper-slide img {
+      width: 100%;
+    }
+
+    .media-object img {
+      width: 100%;
+    }
+
+    .media-object > div > p {
+      border-bottom: 1px dashed #000;
+    }
+
+    .information span {
+      padding-right: 30px;
+    }
+
+    .information span:first-child {
+      font-weight: bold;
+    }
+  </style>
+@endsection
+
+@section('content')
+  <div class="top-bar hide-for-small-only">
+    <div>
+      <div class="top-bar-left">
+        <ul class="dropdown menu" data-dropdown-menu>
+          <li class="menu-text">mime</li>
+          <li v-for="left in top_bar_left"><a href="@{{left.href}}">@{{left.name}}</a></li>
+        </ul>
+      </div>
+      <div class="top-bar-right">
+        <ul class="dropdown menu" data-dropdown-menu>
+          <li v-for="right in top_bar_right"><a href="@{{right.href}}">@{{right.name}}</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" v-for="slide in swiper_pictures">
+          <a href="@{{slide.href}}">
+            <img :src="slide.image" alt="@{{slide.name}}">
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br>
+  <div class="row information">
+    <span v-for="header in main_class.header">@{{ header }}</span>
+
+    <hr>
+    <div class="media-object">
+      <div class="media-object-section medium-6 small-12 columns">
+        <img class="thumbnail" :src="main_class.body.image" alt="@{{ main_class.body.title }}">
+      </div>
+      <div class="media-object-section medium-6 small-12 columns">
+        <h4>@{{ main_class.body.title }}</h4>
+        <p>@{{ main_class.body.paragraph }}<br><br></p>
+        <div class="medium-6 small-12 columns" v-for=" footer in main_class.footer">
+          <p><i class="fa fa-@{{ footer.fa }}"></i>@{{ footer.title }}：@{{ footer.content }}</p>
+        </div>
+        <div class="medium-6 small-12 columns">
+          <button type="button" class="expanded button">课程注册</button>
+        </div>
+        <div class="medium-6 small-12 columns">
+          <p>@{{ main_class.other_information }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row collapse">
+    <ul class="tabs" data-tabs id="example-tabs">
+      <li class="tabs-title" v-for="tab in tabs"><a href="#panel@{{ $index }}">@{{ tab.name }}</a></li>
+    </ul>
+    <div class="tabs-content" data-tabs-content="example-tabs">
+      <div class="tabs-panel is-active" id="panel1">
+        <div class="row" v-for="row in tabs[0].content">
+          <div class="medium-4 small-12 columns">
+            <div class="small-12">
+              <img :src="row.teacher.image" alt="">
+            </div>
+            <div class="small-12">
+              <p>讲师：@{{ row.teacher.title }}</p>
+              <p>课程简介：@{{ row.teacher.brief }}</p>
+            </div>
+          </div>
+          <div class="medium-8 small-12 columns">
+            <div class="medium-4 small-6 columns" v-for="course in row.courses">
+              <div class="small-12">
+                <img :src="course.image" alt="">
+              </div>
+              <div class="small-12">
+                <p>@{{ course.title }}</p>
+                <p>
+                <div class="columns" v-for="span in course.information"><i
+                    class="fa fa-@{{ span.fa }}"></i><span>@{{ span.title }}：@{{ span.content }}</span></div>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="tabs-panel" id="panel2">
+        234234
+      </div>
+      <div class="tabs-panel" id="panel3">
+        8798
+      </div>
+    </div>
+  </div>
+
+@endsection
+
+
+@section('js')
+  <script src="/vendor/swiper/swiper-3.3.0.min.js"></script>
+  <script>
+    vm = new Vue({
+      el: '#open_course',
+      data: {
+        top_bar_left: [
+          {
+            name: '首页',
+            href: 'index'
+          }, {
+            name: '课程',
+            href: '#'
+          }, {
+            name: '讲座',
+            href: '#'
+          }, {
+            name: '病例讲座',
+            href: '#'
+          }, {
+            name: '直播',
+            href: '#'
+          }
+        ],
+
+        top_bar_right: [
+          {
+            name: '登录',
+            href: '#'
+          }, {
+            name: '注册',
+            href: '#'
+          }
+        ],
+
+        swiper_pictures: [
+          {
+            name: '',
+            image: '/image/轮播test.jpg',
+            href: '#'
+          }, {
+            name: '',
+            image: '/image/轮播test.jpg',
+            href: '#'
+          }, {
+            name: '',
+            image: '/image/轮播test.jpg',
+            href: '#'
+          }
+        ],
+
+        main_class: {
+          header: ['2016年甲状腺公开课', '更新至2016-07-16期', '播放总次数：3.87万', ' 问答:156条'],
+
+          body: {
+            title: '课程介绍',
+            image: '/image/test.jpg',
+            paragraph: '2016年甲状腺公开课由国内知名的移动医学教育平台迈德科技发起，邀请国内资深医学专家，通过免费接听语音电话或登陆网络直播的形式，为甲状腺疾病相关领域的内分泌、外科、核医学科等医生，分享最新规范化诊疗经验的系统课程。啊上课大家好请问I诶我空间啊和空间的贺卡上卡还是空间好卡的酒红色空间哈开始卡就是的好看。'
+          },
+
+          footer: [{
+            title: '更新时间',
+            fa: 'clock-o',
+            content: '每周四下午16：00'
+          }, {
+            title: '开课时间',
+            fa: 'video-camera',
+            content: '2016年4月1日'
+          }, {
+            title: '学习时间',
+            fa: 'calendar',
+            content: '2016年4月-12月31日'
+          }, {
+            title: '课程进度',
+            fa: 'paperclip',
+            content: '已播出10期（共21期 ）'
+          }],
+
+          other_information: '已有4596人注册了甲状腺公开课'
+        },
+
+        tabs: [
+          {
+            name: '课程内容',
+            content: [{
+              teacher: {
+                title: '甲亢专题',
+                image: '',
+                teacher_name: '施秉银',
+                brief: '啊可是觉得哈看几乎是看，到阿克苏可千万模拟器。比我年轻比我们那边全面把握强化可惜很快就这款车I我IU去I，请和我快回去看见我会看败。',
+              },
+              courses: [
+                {
+                  title: '第1期：甲亢临床治疗的回顾',
+                  image: '/image/test.jpg',
+                  information: [
+                    {
+                      title: '学习',
+                      fa: 'youtube-play',
+                      content: '2689人'
+                    }, {
+                      title: '',
+                      fa: 'comment',
+                      content: '4条问题'
+                    }
+                  ]
+                }
+              ]
+            }
+            ]
+          }
+        ]
+      }
+    });
+
+    var swiper = new Swiper('.swiper-container', {
+      autoHeight: true,
+      fade: {
+        crossFade: true
+      }
+    });
+
+    $(function () {
+      $('.tabs-title').eq(0).addClass('is-active');
+      $('.tabs-title').eq(0).children('a').attr('aria-selected', "true");
+    })
+
+  </script>
+@endsection
