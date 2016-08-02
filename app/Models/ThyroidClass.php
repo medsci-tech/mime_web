07743b8afc72f7a36b9f37f220fc14e67002d57f
Damon <whliujun@hotmail.com>
2016-08-02 16:58:01 +0800

    thyroidClass models

---
+++ app/Models/ThyroidClass.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\ThyroidClass\ThyroidClassPhase;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class ThyroidClass
+ * @package App\Models
+ * @mixin \Eloquent
+ */
+class ThyroidClass extends Model
+{
+    //
+    protected $table = 'thyroid_class';
+
+}
