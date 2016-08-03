49908d474f74ba44b2867eac32c7bf7586e79343
余鹏飞 <501254061@qq.com>
2016-08-03 17:49:19 +0800

    test

--- resources/views/thyroid-class/course/view.blade.php
+++ resources/views/thyroid-class/course/view.blade.php
@@ -5,6 +5,7 @@
 @section('page_id','open_course')
 
 @section('css')
+  <link rel="stylesheet" href="/vendor/swiper/swiper-3.3.0.min.css">
   <link rel="stylesheet" href="/css/thyroid-class.css">
 @endsection
 
@@ -28,22 +29,53 @@
   </div>
 
   <div class="row">
-    <div class="small-12">
-      <img src="/image/test.jpg" style="width: 100%;height: 200px" alt="">
-    </div>
-  </div>
-  <br>
-
-  <div class="row" style="background-color: #555; padding-top: 1rem; padding-bottom: 1rem;">
-
-    <div class=" medium-7 small-12 columns">
-      <div id="id_video_container" style="width:100%;height:400px;"></div>
-    </div>
-    <div class="medium-5 small-12 columns">
-
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" v-for="slide in swiper_pictures">
+          <a href="@{{slide.href}}">
+            <img :src="slide.image" alt="@{{slide.name}}">
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 
+  <div class="row video">
+
+    <div class="medium-8 small-12 columns">
+      <div id="id_video_container" style="width:100%;"></div>
+    </div>
+    <div class="medium-4 small-12 columns video-list">
+      <h5>&nbsp;课程列表</h5>
+      <ul class="vertical menu" data-accordion-menu>
+        <li v-for="subject in course_list">
+          <a href="#">@{{ subject.subject }}</a>
+          <ul class="menu vertical nested">
+            <li v-for="course in subject.courses"><a href="@{{ course.href }}">@{{ course.name }}</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="footer">
+    <div class="row">
+      <div class="small-8 columns">
+        <dl class="">
+          <dd>&emsp;</dd>
+          <dd>关于我们丨全科医学协作平台简介丨联系方式丨相关法律</dd>
+          <dd>&emsp;</dd>
+          <dd>Copyright © 2016 Phoenix New Media Limited All Rights Reserved.</dd>
+          <dd>空中课堂所有学习视频课适用于《中华人民共和国著作权法》</dd>
+          <dd>空中课堂所有学习视频课经授课专家许可使用，Mime、Itangyi、空课APP经版权方可使用。</dd>
+          <dd>除非另有声明，本平台其他视频作品采用知识共享署名-非商业性使用-相同方式共享。</dd>
+        </dl>
+      </div>
+      <div class="small-4 columns">
+        <img src="/image/全科医学协作平台.jpg" alt="">
+      </div>
+    </div>
+  </div>
 
 
 @endsection
@@ -84,6 +116,48 @@
           }
         ],
 
+        swiper_pictures: [
+          {
+            name: '',
+            image: '/image/轮播test.jpg',
+            href: '#'
+          }, {
+            name: '',
+            image: '/image/轮播test.jpg',
+            href: '#'
+          }, {
+            name: '',
+            image: '/image/轮播test.jpg',
+            href: '#'
+          }
+        ],
+
+        course_list:[
+          {
+            subject: '甲亢专题',
+            courses: [
+              {
+                name: '第一期 xxxx',
+                href: '#'
+              }, {
+                name: '第二期 xxxx',
+                href: '#'
+              },
+            ]
+          }, {
+            subject: '甲减专题',
+            courses: [
+              {
+                name: '第一期 xxxx',
+                href: '#'
+              }, {
+                name: '第二期 xxxx',
+                href: '#'
+              },
+            ]
+          },
+        ]
+
       }
     });
 
@@ -101,11 +175,13 @@
         "auto_play": "0",
         "file_id": "14651978969263009936",
         "app_id": "1252201500",
-        "width": 640,
-        "height": 400
+        "width": 960,
+        "height": 600
       };
       /*调用播放器进行播放*/
       new qcVideo.Player(/*代码中的id_video_container将会作为播放器放置的容器使用,可自行替换*/ "id_video_container", option);
     })()
+
+    $('.video-list').css('height',$('.video-list').prev().height());
   </script>
 @endsection
