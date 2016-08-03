91d8f777d93d5b7a9f8141dfe3f2d3ac9c1a67ae
余鹏飞 <501254061@qq.com>
2016-08-03 16:26:43 +0800

    test

--- resources/views/thyroid-class/course/view.blade.php
+++ resources/views/thyroid-class/course/view.blade.php
@@ -5,90 +5,85 @@
 @section('page_id','open_course')
 
 @section('css')
-  <link rel="stylesheet" href="/vendor/foundation-6.2.3-complete/css/foundation.min.css">
-  <link rel="stylesheet" href="/vendor/font-awesome-4.6.2/css/font-awesome.min.css">
-  <link rel="stylesheet" href="/vendor/video.js/dist/video-js.min.css">
-  <link rel="stylesheet" href="/vendor/swiper/swiper-3.3.0.min.css">
-  <style>
-    .swiper-slide img {
-      width: 100%;
-    }
-    .media-object img {
-      width: 100%;
-    }
-    .media-object>div>p {
-      border-bottom: 1px dashed #000;
-    }
-    .information span {
-      padding-right: 30px;
-    }
-  </style>
+  <link rel="stylesheet" href="/css/thyroid-class.css">
 @endsection
 
 @section('content')
-  <div class="top-bar hide-for-small-only">
-    <div>
-      <div class="top-bar-left">
-        <ul class="dropdown menu" data-dropdown-menu>
-          <li class="menu-text">mime</li>
-          <li v-for="(name,href) in top_bar_right"><a href="@{{href}}">@{{name}}</a></li>
-        </ul>
-      </div>
-      <div class="top-bar-right">
-        <ul class="dropdown menu" data-dropdown-menu>
-          <li v-for="(name,href) in top_bar_left"><a href="@{{href}}">@{{name}}</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
   <div class="row">
-    <div class="swiper-container">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide" v-for="(name,href) in swiper_pictures">
-          <a href="@{{href[0]}}">
-            <img :src="href[1]" alt="@{{name}}">
-          </a>
+    <div class="top-bar">
+      <div>
+        <div class="top-bar-left">
+          <ul class="dropdown menu" data-dropdown-menu>
+            <li><img src="/image/logo.jpg" alt=""></li>
+            <li v-for="left in top_bar_left"><a href="@{{left.href}}">@{{left.name}}</a></li>
+          </ul>
+        </div>
+        <div class="top-bar-right">
+          <ul class="dropdown menu" data-dropdown-menu>
+            <li v-for="right in top_bar_right"><a href="@{{right.href}}">@{{right.name}}</a></li>
+          </ul>
         </div>
       </div>
     </div>
   </div>
-  <br>
+
   <div class="row">
-    <div class="medium-7 small-12">
-      <video id="example_video_1" class="video-js vjs-default-skin"
-             controls preload="auto" style="width: 100%;height: 500px"
-             poster=""
-             data-setup='{"example_option":true}'>
-        <source type="video/mp4" src="http://7xrlyr.com1.z0.glb.clouddn.com/%E4%BD%A0%E4%BC%9A%E7%9B%91%E6%B5%8B%E8%A1%80%E7%B3%96%E5%90%97%EF%BC%9F.mp4">
-      </video>
+    <div class="small-12">
+      <img src="/image/test.jpg" style="width: 100%;height: 200px" alt="">
     </div>
   </div>
+  <br>
+
+  <div class="row" style="background-color: #555; padding-top: 1rem; padding-bottom: 1rem;">
+
+    <div class=" medium-7 small-12 columns">
+      <div id="id_video_container" style="width:100%;height:400px;"></div>
+    </div>
+    <div class="medium-5 small-12 columns">
+
+    </div>
+  </div>
+
+
 
 @endsection
 
 
 @section('js')
   <script src="/vendor/swiper/swiper-3.3.0.min.js"></script>
-  <script src="/vendor/video.js/dist/video.min.js"></script>
   <script>
     vm = new Vue({
       el: '#open_course',
       data: {
-        top_bar_right: {
-          '首页': 'index',
-          '课程': '#',
-          '讲座': '#',
-          '病例讲座': '#',
-          '直播': '#'
-        },top_bar_left: {
-          '登录': 'index',
-          '注册': '#'
-        },
-        swiper_pictures: {
-          '轮播图1': ['#', '/image/轮播test.jpg'],
-          '轮播图2': ['#', '/image/轮播test.jpg'],
-          '轮播图3': ['#', '/image/轮播test.jpg']
-        }
+        top_bar_left: [
+          {
+            name: '首页',
+            href: 'index'
+          }, {
+            name: '课程',
+            href: '#'
+          }, {
+            name: '讲座',
+            href: '#'
+          }, {
+            name: '病例讲座',
+            href: '#'
+          }, {
+            name: '直播',
+            href: '#'
+          }
+        ],
+
+        top_bar_right: [
+          {
+            name: '登录',
+            href: '#'
+          }, {
+            name: '注册',
+            href: '#'
+          }
+        ],
+
       }
     });
 
@@ -99,5 +94,18 @@
       }
     });
 
+  </script>
+  <script src="http://qzonestyle.gtimg.cn/open/qcloud/video/h5/h5connect.js"></script>
+  <script type="text/javascript"> (function () {
+      var option = {
+        "auto_play": "0",
+        "file_id": "14651978969263009936",
+        "app_id": "1252201500",
+        "width": 640,
+        "height": 400
+      };
+      /*调用播放器进行播放*/
+      new qcVideo.Player(/*代码中的id_video_container将会作为播放器放置的容器使用,可自行替换*/ "id_video_container", option);
+    })()
   </script>
 @endsection
