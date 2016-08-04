d5b9edcc826ce7cc6a7fa377d19607df3827ce0d
Damon <whliujun@hotmail.com>
2016-08-04 17:40:14 +0800

    validator

--- resources/views/home/register/create.blade.php
+++ resources/views/home/register/create.blade.php
@@ -5,56 +5,68 @@
 @section('page_id','sign_up')
 
 @section('css')
-  <style>
-    .log-in-form {
-      border: 1px solid #cacaca;
-      padding: 1rem 1rem !important;
-      border-radius: 3px;
-    }
+    <style>
+        .log-in-form {
+            border: 1px solid #cacaca;
+            padding: 1rem 1rem !important;
+            border-radius: 3px;
+        }
 
-    .help-text {
-      color: #ec5840;
-    }
+        .help-text {
+            color: #ec5840;
+        }
 
-    textarea:disabled {
-      height: 6rem;
-      overflow-y: auto;
-      cursor: inherit;
-      background-color: #fefefe;
-    }
-  </style>
+        textarea:disabled {
+            height: 6rem;
+            overflow-y: auto;
+            cursor: inherit;
+            background-color: #fefefe;
+        }
+    </style>
 @endsection
 
 @section('content')
-  <div class="row">
-    <div class="medium-6 medium-centered large-4 large-centered columns">
-      <br>
-      <form method="post" action="/home/register/store">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
-        <div class="row column log-in-form">
-          <h4 class="text-center">Mime账号注册</h4>
-          <label>手机号
-            <input v-model="phone" type="number" placeholder="请输入您的手机号" name="phone">
-          </label>
-          <p id="error_phone" class="help-text hide">您输入的手机号码有误</p>
-          <label>验证码
-            <div class="input-group">
-              <input v-model="sms" class="input-group-field" type="text" placeholder="请输入验证码" name="auth_code">
-              <div class="input-group-button">
-                <button @click="get_auth_code" type="button" class="button">获取验证码</button>
-              </div>
-            </div>
-          </label>
-          <p id="error_sms" class="help-text hide">您输入的验证码有误</p>
-          <label>密码
-            <input v-model="password" type="password" placeholder="请输入密码" name="password">
-          </label>
-          <label>确认密码
-            <input v-model="password2" type="password" placeholder="请再次输入密码" name="password2">
-          </label>
-          <p id="error_passord" class="help-text hide">两次输入的密码不一致</p>
+    <div class="row">
+        <div class="medium-6 medium-centered large-4 large-centered columns">
+            <br>
 
-          <div style="font-size: .875rem">用户协议
+            <form method="post" action="/home/register/store">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
+
+                <div class="row column log-in-form">
+                    <h4 class="text-center">Mime账号注册</h4>
+                    <label>手机号
+                        <input v-model="phone" type="number" placeholder="请输入您的手机号" name="phone">
+                    </label>
+                    @if($errors->has('phone'))
+                        <p id="error_phone" class="help-text">{{ $errors->first('phone')}}</p>
+                    @endif
+                    <label>验证码
+                        <div class="input-group">
+                            <input v-model="sms" class="input-group-field" type="text" placeholder="请输入验证码"
+                                   name="auth_code">
+
+                            <div class="input-group-button">
+                                <button @click="get_auth_code" type="button" class="button">获取验证码</button>
+                            </div>
+                        </div>
+                    </label>
+
+                    @if($errors->has('auth_code'))
+                        <p id="error_sms" class="help-text">{{ $errors->first('auth_code')}}</p>
+                    @endif
+                    <label>密码
+                        <input v-model="password" type="password" placeholder="请输入密码" name="password">
+                    </label>
+                    <label>确认密码
+                        <input v-model="password2" type="password" placeholder="请再次输入密码" name="password2">
+                    </label>
+
+                    @if($errors->has('password'))
+                        <p id="error_passord" class="help-text">{{ $errors->first('password')}}</p>
+                    @endif
+
+                    <div style="font-size: .875rem">用户协议
             <textarea disabled>
 第1条关于本协议
 
@@ -159,67 +171,68 @@
 9.2 迈德同信尊重用户和消费者的合法权利，本协议及教育平台上发布的各类规则、声明等其他内容，均是为了更好的、更加便利的为用户提供服务。教育平台欢迎用户和社会各界提出意见和建议，迈德同信将虚心接受并适时修改本协议及教育平台的各类规则。
 9.3 您点击本协议上方的“同意以下协议，提交”按钮即视为您完全接受本协议，在点击之前请您再次确认已知悉并完全理解本协议的全部内容。
             </textarea>
-          </div>
+                    </div>
 
-          <br>
-          <p>
-            <button type="submit" class="button expanded">注&emsp;册</button>
-          </p>
-          <p class="text-center"><a href="/home/login">已有账号?点击登录</a></p>
+                    <br>
+
+                    <p>
+                        <button type="submit" class="button expanded">注&emsp;册</button>
+                    </p>
+                    <p class="text-center"><a href="/home/login">已有账号?点击登录</a></p>
+                </div>
+            </form>
+
         </div>
-      </form>
-
     </div>
-  </div>
 @endsection
 
 
 @section('js')
-  <script>
-    vm = new Vue({
-      el: '#sign_up',
-      data: {
-        phone: '',
-        sms: '',
-        password: '',
-        password2: ''
-      },
-      methods: {
-        get_auth_code: function () {
+    <script>
+        vm = new Vue({
+            el: '#sign_up',
+            data: {
+                phone: '',
+                sms: '',
+                password: '',
+                password2: ''
+            },
+            methods: {
+                get_auth_code: function () {
 
-          $('error_phone').addClass('hide');
-          $('.input-group-button button').attr("disabled", "disabled");
+                    $('error_phone').addClass('hide');
+                    $('.input-group-button button').attr("disabled", "disabled");
 
-          var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
-          if (myreg.test(vm.phone)) {
-            $.get('/home/register/sms', {phone: vm.phone}, function (data) {
-                if (data.success) {
-                } else {
-                  $('#error_phone').text('手机号已被注册');
-                  $('#error_phone').removeClass('hide')
+                    var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
+                    if (myreg.test(vm.phone)) {
+                        $.get('/home/register/sms', {phone: vm.phone}, function (data) {
+                                    if (data.success) {
+                                    } else {
+                                        $('#error_phone').text('手机号已被注册');
+                                        $('#error_phone').removeClass('hide')
+                                    }
+                                }
+                        );
+                    } else {
+                        $('error_phone').removeClass('hide')
+                    }
+
+
+                    var i = 61;
+                    timer();
+                    function timer() {
+                        i--;
+                        $('.input-group-button button').text(i + '秒后重发');
+                        if (i == 0) {
+                            clearTimeout(timer);
+                            $('.input-group-button button').removeAttr("disabled");
+                            $('.input-group-button button').text('重新发送');
+                        } else {
+                            setTimeout(timer, 1000);
+                        }
+                    }
                 }
-              }
-            );
-          } else {
-            $('error_phone').removeClass('hide')
-          }
-
-
-          var i = 61;
-          timer();
-          function timer() {
-            i--;
-            $('.input-group-button button').text(i + '秒后重发');
-            if (i == 0) {
-              clearTimeout(timer);
-              $('.input-group-button button').removeAttr("disabled");
-              $('.input-group-button button').text('重新发送');
-            } else {
-              setTimeout(timer, 1000);
             }
-          }
-        }
-      }
-    });
-  </script>
+        });
+    </script>
 @endsection
