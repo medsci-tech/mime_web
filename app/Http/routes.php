c258e763c62523fd81c11fe28b91c41bbb0b3b04
Damon <whliujun@hotmail.com>
2016-08-04 15:21:08 +0800

    fix

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/index', 'ThyroidClass/ThyroidClassController@index');
 
 
 Route::group(['prefix' => 'home', 'namespace' => 'Home'], function () {
