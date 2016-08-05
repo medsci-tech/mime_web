e07effb33e8f7bd249fb86855717af96a50dc72c
Damon <whliujun@hotmail.com>
2016-08-05 12:03:24 +0800

    fix

--- app/Http/Controllers/Home/LoginController.php
+++ app/Http/Controllers/Home/LoginController.php
@@ -55,8 +55,23 @@ class LoginController extends Controller
         } /*if>*/
 
         \Session::set('studentId', $student->id);
-        $this->middleware('replenish');
-        return redirect('/');
+        if ($student->name
+            && $student->sex
+            && $student->email
+            && $student->birthday
+            && $student->office
+            && $student->title
+            && $student->province
+            && $student->city
+            && $student->area
+            && $student->hospital_name
+        ) {
+            \Session::set('replenished', true);
+            return redirect('/');
+        } else {
+            return redirect('/home/replenish/create');
+        }
+
     }
 
     /**
