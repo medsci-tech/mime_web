b8cb244a7af3c52b0f54370a94046bfb7714bc76
Damon <whliujun@hotmail.com>
2016-08-05 19:06:18 +0800

    fix

--- app/Helpers/Statistics/Statistics.php
+++ app/Helpers/Statistics/Statistics.php
@@ -3,7 +3,6 @@
 namespace App\Helpers\Statistics;
 
 use App\Models\ThyroidClass\ThyroidClassCourse;
-use App\Models\ThyroidClassPhase;
 
 /**
  * Class Statistics
@@ -14,23 +13,23 @@ class Statistics
     /**
      * @param $course
      */
-    protected function updateCourseCount($courseId)
+    protected function updateCourseCount($course)
     {
-        $course = ThyroidClassCourse::find($courseId);
         $course->play_count += 1;
         $course->save();
     }
 
-    protected function updatePhaseCount($courseId)
+    protected function updatePhaseCount($course)
     {
-        $course = ThyroidClassCourse::find($courseId);
-        $phase = ThyroidClassPhase::find($course->phase_id)->play_count += 1;
+        $phase = $course->thyroidClassPhase;
+        $phase->play_count += 1;
         $phase->save();
     }
 
     public function updateCount($courseId)
     {
-        $this->updateCourseCount($courseId);
-        $this->updatePhaseCount($courseId);
+        $course = ThyroidClassCourse::find($courseId);
+        $this->updateCourseCount($course);
+        $this->updatePhaseCount($course);
     }
 } /*class*/
