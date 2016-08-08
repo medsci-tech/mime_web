e6c931544f34eca3582c7a399ee59cfca73f417c
Damon <whliujun@hotmail.com>
2016-08-08 17:08:02 +0800

    fix

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,14 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Company
+ * @package App\Models
+ */
 class Company extends Model
 {
-    //
+    /**
+     * @var string
+     */
     protected $table = 'companies';
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function representatives()
     {
-        return $this->hasMany('App\Models\Representative', 'company_id');
+        return $this->hasMany(Representative::class, 'company_id');
     }
 
 }
