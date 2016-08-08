3c12d7a74f4412d4cb9feca6df4cb483567cd2ca
Damon <whliujun@hotmail.com>
2016-08-08 16:17:40 +0800

    admin auth

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+
+class AdminController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.index');
+    }
+}
