0d9e3940346227e8f80a587d1fd5e0c64c93f6c0
Damon <whliujun@hotmail.com>
2016-08-12 11:32:10 +0800

    fix

--- database/migrations/2016_08_10_175208_add_entered_at_to_students_tables.php
+++ database/migrations/2016_08_10_175208_add_entered_at_to_students_tables.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddEmailAndEnteredAtToStudentsTables extends Migration
+class AddEnteredAtToStudentsTables extends Migration
 {
     /**
      * Run the migrations.
