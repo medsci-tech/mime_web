54845849cc06724be44e408a0b764dff58e7d5da
Damon <whliujun@hotmail.com>
2016-08-16 20:46:46 +0800

    fix

--- resources/views/admin/student/logs.blade.php
+++ resources/views/admin/student/logs.blade.php
@@ -60,7 +60,8 @@
                                     <label for="entered_at" class="col-md-4 control-label">观看时间：{{$key}}</label>
 
                                     <div class="col-md-6">
-                                        <input type="text" name="excel" class="form-control" value="观看时长：{{$value}}"
+                                        <input type="text" name="excel" class="form-control" value='"观看时长：{{{
+                                        $arr = getdate($value);echo $arr[2]."小时".$arr[1]."分钟".$arry[0]."秒";}}}'
                                                readonly>
                                     </div>
                                 </div>
