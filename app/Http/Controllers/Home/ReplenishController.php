d13eedcf74f18f6afa94242b3e24fe53f1c38390
Damon <whliujun@hotmail.com>
2016-08-25 16:09:22 +0800

    fix

--- app/Http/Controllers/Home/ReplenishController.php
+++ app/Http/Controllers/Home/ReplenishController.php
@@ -31,14 +31,12 @@ class ReplenishController extends WebController
      */
     public function create()
     {
-        if (!$this->studentId)
-        {
+        if (!$this->studentId) {
             return redirect('home/replenish/error');
         } /*if>*/
 
         $student = Student::where('id', '=', $this->studentId)->first();
-        if (!$student) 
-        {
+        if (!$student) {
             return redirect('home/replenish/error');
         } /*if>*/
         $student->birthday = date('Y-m-d', strtotime($student->birthday));
@@ -51,14 +49,12 @@ class ReplenishController extends WebController
      */
     public function store(Request $request)
     {
-        if (!$this->studentId)
-        {
+        if (!$this->studentId) {
             return redirect('home/replenish/error');
         } /*if>*/
 
         $student = Student::find($this->studentId);
-        if (!$student)
-        {
+        if (!$student) {
             \Session::clear();
             return redirect('/');
         } /*if>*/
@@ -66,7 +62,7 @@ class ReplenishController extends WebController
         $messages = [
             'name.required' => '姓名未填写',
             'nickname.required' => '姓名未填写',
-//            'sex.required' => '性别未填写',
+            'sex.required' => '性别未填写',
             'email.required' => '邮箱未填写',
             'birthday.required' => '生日未填写',
             'office.required' => '科室未填写',
@@ -77,38 +73,37 @@ class ReplenishController extends WebController
         ];
 
         $rules = [
-            'name'      => 'required',
-            'nickname'      => 'required',
-//            'sex'       => 'required',
-            'email'     => 'required',
-            'birthday'  => 'required',
-            'office'    => 'required',
-            'title'     => 'required',
-            'province'  => 'required',
-            'city'      => 'required',
-            'area'      => 'required',
-            'hospital_name'  => 'required',
+            'name' => 'required',
+            'nickname' => 'required',
+            'sex' => 'required',
+            'email' => 'required',
+            'birthday' => 'required',
+            'office' => 'required',
+            'title' => 'required',
+            'province' => 'required',
+            'city' => 'required',
+            'area' => 'required',
+            'hospital_name' => 'required',
         ];
         $validator = \Validator::make($request->all(), $rules, $messages);
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return redirect()->back()->withErrors($validator->errors())->withInput();
         } /*if>*/
         $student->update([
-            'name'      => $request->input('name'),
-            'nickname'      => $request->input('nickname'),
-            'sex'       => $request->input('sex'),
-            'email'     => $request->input('email'),
-            'birthday'  => $request->input('birthday'),
-            'office'    => $request->input('office'),
-            'title'     => $request->input('title'),
-            'province'  => $request->input('province'),
-            'city'      => $request->input('city'),
-            'area'      => $request->input('area'),
+            'name' => $request->input('name'),
+            'nickname' => $request->input('nickname'),
+            'sex' => $request->input('sex'),
+            'email' => $request->input('email'),
+            'birthday' => $request->input('birthday'),
+            'office' => $request->input('office'),
+            'title' => $request->input('title'),
+            'province' => $request->input('province'),
+            'city' => $request->input('city'),
+            'area' => $request->input('area'),
             'hospital_level' => $request->input('hospital_level'),
-            'hospital_name'  => $request->input('hospital_name'),
+            'hospital_name' => $request->input('hospital_name'),
         ]);
-        \Session::set('replenished' , true);
+        \Session::set('replenished', true);
         return redirect('/');
     }
 
