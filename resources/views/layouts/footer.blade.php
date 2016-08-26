3267b4ba9a6ea685857526d6cf96bea3fe8f8b7d
余鹏飞 <501254061@qq.com>
2016-08-26 17:44:33 +0800

    test

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,17 +1,17 @@
 <div class="footer">
   <div class="row">
     <div class="medium-8 small-12 columns">
-      <dl class="">
+      <dl class="" style="font-size: 0.8rem">
         <dd>&emsp;</dd>
-        <dd>Copyright © Medscience-tech.All rights reserved.鄂ICP备13013615号-1</dd>
+        <dd><p>Copyright © Medscience-tech.All rights reserved.鄂ICP备13013615号-1</p></dd>
         {{--<dd>&emsp;</dd>--}}
-        <dd style="color: #666">所有学习视频课适用于《中华人民共和国著作权法》</dd>
-        <dd style="color: #666">所有学习视频课经授课专家许可使用，Mime、医师助手APP经版权方可使用。</dd>
-        <dd style="color: #666">除非另有声明，本平台其他视频作品采用知识共享署名-非商业性使用-相同方式共享2.5中国大陆许可协议进行许可</dd>
+        <dd style="color: #666; margin-bottom: .5rem;">本平台所有学习视频课件适用于《中华人民共和国著作权法》</dd>
+        <dd style="color: #666; margin-bottom: .5rem;">本平台所有学习视频课件经授课专家授权使用，未经书面允许，请勿转播</dd>
+        <dd style="color: #666; margin-bottom: .5rem;">除非另有声明，本平台其它视频作品采用Creative Commons知识共享署名-非商业性使用-相同方式共享2.5中国大陆许可协议进行许可</dd>
       </dl>
     </div>
-    <div class="medium-4 small-12 columns">
-      <img src="/image/迈德科技.png" alt="">
+    <div class="medium-4 small-12 columns" style="text-align: center">
+      <img style="width: 300px" src="/image/迈德科技.png" alt="">
     </div>
   </div>
 </div>
