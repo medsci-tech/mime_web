f2c0be84c0e5a0ddd551612e88a11f428e391819
余鹏飞 <501254061@qq.com>
2016-08-26 09:46:45 +0800

    test

--- resources/views/thyroid-class/index.blade.php
+++ resources/views/thyroid-class/index.blade.php
@@ -246,23 +246,7 @@
     </div>
   </div>
 
-  <div class="footer">
-    <div class="row">
-      <div class="medium-8 small-12 columns">
-        <dl class="">
-          <dd>&emsp;</dd>
-          <dd>Copyright © Medscience-tech.All rights reserved.鄂ICP备13013615号-1</dd>
-          <dd>&emsp;</dd>
-          <dd>所有学习视频课适用于《中华人民共和国著作权法》</dd>
-          <dd>所有学习视频课经授课专家许可使用，Mime、医师助手APP经版权方可使用。</dd>
-          <dd>除非另有声明，本平台其他视频作品采用知识共享署名-非商业性使用-相同方式共享2.5中国大陆许可协议进行许可</dd>
-        </dl>
-      </div>
-      <div class="medium-4 small-12 columns">
-        <img src="/image/迈德科技.png" alt="">
-      </div>
-    </div>
-  </div>
+  @include('layouts.footer')
 
 @endsection
 
