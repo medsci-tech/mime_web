a192e2704367da00d53846b1aba2b4f9b222fe9c
Damin09323 <0932313lj>
2016-09-01 11:23:58 +0800

    fix

--- database/migrations/2016_09_01_175208_create_banners_tables.php
+++ database/migrations/2016_09_01_175208_create_banners_tables.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBannersTable extends Migration
+class CreateBannersTables extends Migration
 {
     /**
      * Run the migrations.
