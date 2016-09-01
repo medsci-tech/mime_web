092c465c85b5e31c85954a56a702906243fb5520
Damin09323 <0932313lj>
2016-09-01 11:25:14 +0800

    fix

--- database/migrations/2016_09_01_175208_create_banners_tables.php
+++ database/migrations/2016_09_01_175208_create_banners_tables.php
@@ -17,6 +17,7 @@ class CreateBannersTables extends Migration
             $table->increments('id')->comment('主键.');
             $table->string('image_url')->comment('banner图片地址.');
             $table->string('href_url')->comment('链接地址.');
+            $table->string('page')->comment('所属页面.');
             $table->tinyInteger('status')->default(0)->comment('是否显示 0 为不显示 1为显示.');
             $table->timestamps();
         });
