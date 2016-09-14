ec21c8b3bb2395c498dcd5a90066ca6f4ca0e86b
Damon <whliujun@hotmail.com>
2016-09-14 15:54:39 +0800

    fix

--- resources/views/backend/tables/student.blade.php
+++ resources/views/backend/tables/student.blade.php
@@ -7,12 +7,12 @@
 @section('tables_data')
   <script>
     var data = {
-      table_head: ['id', '姓名', '省', '市', '区', '医院', '科室', '职称', '注册时间', '报名时间'],
+      table_head: ['id', '手机号','姓名', '省', '市', '区', '医院', '科室', '职称', '注册时间', '报名时间'],
       log_head: ['课程名称', '点击时间', '观看时长'],
       data: [
         @foreach($students as $student)
         {
-          table_data: ['{{$student->id}}', '{{$student->name}}', '{{$student->province}}', '{{$student->city}}', '{{$student->area}}', '{{$student->hospital_name}}', '{{$student->office}}', '{{$student->title}}', '{{$student->created_at}}', '{{$student->entered_at}}'],
+          table_data: ['{{$student->id}}', '{{$student->phone}}','{{$student->name}}', '{{$student->province}}', '{{$student->city}}', '{{$student->area}}', '{{$student->hospital_name}}', '{{$student->office}}', '{{$student->title}}', '{{$student->created_at}}', '{{$student->entered_at}}'],
           log_data: [
             @foreach($student->playLogs as $log)
               @foreach($log->details as $key => $value)
@@ -26,7 +26,7 @@
       modal_data: '',
       alert: alert,
       @if(isset($search))
-       pagination: '{{$students->appends(['key' => $search])->render() }}'
+        pagination: '{{$students->appends(['key' => $search])->render() }}'
       @else
         pagination: '{{$students->render() }}'
       @endif
