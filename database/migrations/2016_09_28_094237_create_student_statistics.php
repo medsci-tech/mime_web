e957988ef00beb0daab8a6bfa419cdf9dbbc6ec3
Damon <whliujun@hotmail.com>
2016-09-28 10:01:00 +0800

    create statistics migration

---
+++ database/migrations/2016_09_28_094237_create_student_statistics.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentStatistics extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_statistics', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('statistic_term', 255)->nullable()->comment('统计项目名');
+            $table->string('meta_key', 255)->nullable()->comment('元信息关键字');
+            $table->string('meta_value', 255)->nullable()->comment('元信息的详细值');
+            $table->timestamps();
+        });
+
+        Schema::create('cities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('province');
+            $table->string('city')->index();
+            $table->decimal('latitude', 11, 8)->default(0)->comment('城市经度.');
+            $table->decimal('longitude', 11, 8)->default(0)->comment('城市纬度.');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('student_statistics');
+        Schema::drop('cities');
+    }
+}
