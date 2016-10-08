e2a7bef79d7d9708c6db349b5589bc252b7a1ad7
Damon <whliujun@hotmail.com>
2016-10-08 14:41:16 +0800

    fix

--- resources/views/backend/charts/charts_pie_class.blade.php
+++ resources/views/backend/charts/charts_pie_class.blade.php
@@ -52,27 +52,27 @@
               trigger: 'item',
               formatter: "{a} <br/>{b} : {c} ({d}%)"
             },
-            legend: {
-              orient: 'vertical',
-              left: 'left',
-              data: title
-            },
-//            series : [
-//              {
-//                name: '课程统计',
-//                type: 'pie',
-//                radius : '55%',
-//                center: ['50%', '60%'],
-//                data:data,
-//                itemStyle: {
-//                  emphasis: {
-//                    shadowBlur: 10,
-//                    shadowOffsetX: 0,
-//                    shadowColor: 'rgba(0, 0, 0, 0.5)'
-//                  }
-//                }
-//              }
-//            ]
+//            legend: {
+//              orient: 'vertical',
+//              left: 'left',
+//              data: title
+//            },
+            series : [
+              {
+                name: '课程统计',
+                type: 'pie',
+                radius : '55%',
+                center: ['50%', '60%'],
+                data:data,
+                itemStyle: {
+                  emphasis: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                  }
+                }
+              }
+            ]
           }
         }
       }
