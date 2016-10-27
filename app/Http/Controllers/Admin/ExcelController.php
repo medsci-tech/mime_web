3d16d728c52ade4cad8b623eebba18c69f94f4c9
Damon <whliujun@hotmail.com>
2016-10-27 09:58:17 +0800

    fix

--- app/Http/Controllers/Admin/ExcelController.php
+++ app/Http/Controllers/Admin/ExcelController.php
@@ -187,8 +187,8 @@ class ExcelController extends Controller
         $studentCourseIds = \Redis::command('keys', ['student_course_id*']);
         $cellData = [['单元名称', '课程名称', '学员姓名',  '学员电话', '起始观看时间', '观看时长(单位/秒)']];
         foreach($studentCourseIds as $studentCourseId) {
+            echo $studentCourseId.'<hr />';
             $logs = \Redis::command('HGETAll', [$studentCourseId]);
-            echo $logs.'<hr />';
             $logArray = explode('-' ,substr($studentCourseId, strpos($studentCourseId, ':')+1));
             foreach($logs as $key => $value) {
                 if($key > '2016-09-08 00:00:00' && $value > 7200) {
