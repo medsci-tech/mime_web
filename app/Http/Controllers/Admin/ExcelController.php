7e4bb6f7d6470b82659e600847a0ffa3de04cfa1
Damon <whliujun@hotmail.com>
2016-11-08 14:45:25 +0800

    fix

--- app/Http/Controllers/Admin/ExcelController.php
+++ app/Http/Controllers/Admin/ExcelController.php
@@ -250,8 +250,8 @@ class ExcelController extends Controller
         $cellData = [['单元名称', '课程名称', '学员姓名', '学员电话', '起始观看时间', '观看时长(单位/秒)']];
         foreach ($playLogs as $playLog) {
             //echo $studentCourseId.'<hr />';
-            dd($playLog->detail);
-            foreach ($playLog->detail as $key => $value) {
+            dd($playLog->details);
+            foreach ($playLog->details as $key => $value) {
                 $item = [
                     $coursesArray[$playLog->thyroid_class_course_id]['phase'],
                     $coursesArray[$playLog->thyroid_class_course_id]['course'],
