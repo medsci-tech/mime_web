34a509ed2c55a781c52aaf65fcd30e1ae93a8ec6
fantasylxh <772932587@qq.com>
2017-01-23 16:00:05 +0800

    选择redis数据库

--- config/database.php
+++ config/database.php
@@ -105,14 +105,12 @@ return [
     */
 
     'redis' => [
-
         'cluster' => false,
-
         'default' => [
             'host' => env('REDIS_HOST', 'localhost'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => 0,
+            'database' => 1,
         ],
 
     ],
