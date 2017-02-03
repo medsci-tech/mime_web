b29832424eb3d229dba9bd054270c9e2233057c4
fantasylxh <772932587@qq.com>
2017-02-03 12:30:52 +0800

    空课域名路由

--- modules/AirClass/Http/routes.php
+++ modules/AirClass/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 /* 空中课堂子域名路由配置 */
-Route::group(['domain' => 'airclass.mime.org.dev','middleware' => 'web', 'prefix' => '', 'namespace' => 'Modules\AirClass\Http\Controllers'], function()
+Route::group(['domain' => env('AIR_DOMAIN'),'middleware' => 'web', 'prefix' => '', 'namespace' => 'Modules\AirClass\Http\Controllers'], function()
 {
 	Route::get('/', 'AirClassController@index');
 });
