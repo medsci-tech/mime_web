db092a71cfdc8aa73379d84ef1a8d2d989f89317
fantasylxh <772932587@qq.com>
2017-02-03 12:22:28 +0800

    add ignore

--- modules/Wap/Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'wap', 'namespace' => 'Modules\Wap\Http\Controllers'], function()
-{
-	Route::get('/', 'WapController@index');
-});
--- modules/WeiXin/Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'weixin', 'namespace' => 'Modules\WeiXin\Http\Controllers'], function()
-{
-	Route::get('/', 'WeiXinController@index');
-});
