bd5f1719a5d53a4203943fd0d1d090f324e1dc34
fantasylxh <772932587@qq.com>
2017-02-05 14:34:58 +0800

    revert

--- modules/AirClass/Http/Controllers/AirClassController.php
+++ modules/AirClass/Http/Controllers/AirClassController.php
@@ -5,17 +5,6 @@ class AirClassController extends Controller {
 	
 	public function index()
 	{
-	    //test  redis
-        $key = 'user:name:6';
-        $name = 'loose';
-        //将用户名存储到Redis中
-        \Redis::set($key,$name);
-        //判断指定键是否存在
-        if(\Redis::exists($key)){
-            //根据键名获取键值
-            //dd(\Redis::get($key));
-        }
-
 		return view('airclass::index');
 	}
 	
