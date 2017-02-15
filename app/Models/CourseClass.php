f25bae646b91cfc5b35d2c2ec4214078ecbb37e6
fantasylxh <772932587@qq.com>
2017-02-15 15:18:28 +0800

    mod  table name

--- app/Models/CourseClass.php
+++ app/Models/CourseClass.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class CourseClass extends Model
 {
-    protected $table = 'course_class';
+    protected $table = 'course_classes';
 
     protected $fillable = [
         'name',
