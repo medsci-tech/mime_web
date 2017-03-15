743467d928937cdac6ee2a4a6b3db901c1d8c299
lostofland <772932587@qq.com>
2017-03-15 10:33:39 +0800

    add office

---
+++ modules/AirClass/Entities/Office.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chelsea
+ * Date: 2016/4/14
+ * Time: 17:48
+ */
+
+namespace Modules\AirClass\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Office extends Model
+{
+    protected $table = 'office';
+
+}
