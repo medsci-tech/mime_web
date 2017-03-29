27e370ef6647882c689cc24d6342251fd256fad7
lostofland <772932587@qq.com>
2017-03-29 12:31:58 +0800

    添加protected

--- app/Models/KZKTClass.php
+++ app/Models/KZKTClass.php
@@ -6,4 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 class KZKTClass extends Model
 {
     protected $table = 'kzkt_classes';
+    protected $fillable = ['volunteer_id', 'doctor_id', 'type', 'login_number', 'invite_number', 'status', 'created_at', 'updated_at', 'site_id'];
 }
