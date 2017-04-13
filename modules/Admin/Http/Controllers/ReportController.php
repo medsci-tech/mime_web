e1c1213851227d06d993ba4b381480e2e43887c9
zhaiyu <zhaiyu_hb@hotmail.com>
2017-04-13 21:21:09 +0800

    后台添加学员统计报表导出2

--- modules/Admin/Http/Controllers/ReportController.php
+++ modules/Admin/Http/Controllers/ReportController.php
@@ -15,7 +15,7 @@ use Modules\OpenClass\Entities\ThyroidClassCourse;
 class ReportController extends Controller
 {
     protected $site_id = 2; // 站点id
-    protected $export_sign_info = 10; // 每页条数
+    protected $export_sign_info = 100; // 每页条数
     protected $public_class_id = 4; // 公开课id
     protected $answer_class_id = 2; // 答疑课id
 
@@ -27,9 +27,7 @@ class ReportController extends Controller
         ]);
     }
 
-    public function export2ExcelByProvince(){
-        set_time_limit(0);
-        $data[] = ['省份','学员总数','总时长', '答疑课', '理论课', '点击量', '等级一', '等级二', '等级三'];
+    public function course_category(){
         $course_lists = [];
         // 课程类别
         $course_categories = CourseClass::where(['site_id' => $this->site_id, 'status' => 1])->get();
@@ -43,10 +41,17 @@ class ReportController extends Controller
             $course_lists[$key]['name'] = $course_category->name;
             $course_lists[$key]['id'] = $course_category->id;
         }
+        return $course_lists;
+    }
+
+    public function export2ExcelByProvince(){
+        set_time_limit(0);
+        $data[] = ['省份','学员总数','总时长', '答疑课', '理论课', '点击量', '等级一', '等级二', '等级三'];
+        $course_lists = $this->course_category();
 //        dd($course_lists);
 
         $hospital_groups = Hospital::groupBy('province')->get();
-        dd($hospital_groups);
+//        dd($hospital_groups);
         foreach ($hospital_groups as $key => $hospital_group){
             $doctor_total = 0; // 医生总数
             // 医生等级
@@ -65,25 +70,30 @@ class ReportController extends Controller
             $click_num = 0;
             $province_hospitals = Hospital::where('province', $hospital_group->province)->get();
             foreach ($province_hospitals as $province_hospital){
-                $hospital_doctors = Doctor::where('hospital_id',$province_hospital->id)->get();
-                $doctor_total += $hospital_doctors->count();
+                if($province_hospital){
+                    $hospital_doctors = Doctor::where('hospital_id',$province_hospital->id)->get();
+                    $doctor_total += $hospital_doctors->count();
 //                dd($hospital_doctors);
-                foreach ($hospital_doctors as $hospital_doctor){
-                    // 等级统计
-                    $doctor_level[$hospital_doctor->rank]++;
-                    // 时长统计
-                    foreach ($course_lists as $course_list){
-                        $study = StudyLog::whereIn('course_id', $course_list['course_ids'])
-                            ->where(['site_id' => $this->site_id, 'doctor_id' => $hospital_doctor->id])
-                            ->get();
-                        if($course_list['id'] == $this->public_class_id){
-                            $study_duration['public'] += $study->sum('study_duration');
-                        }else if($course_list['id'] == $this->answer_class_id){
-                            $study_duration['answer'] += $study->sum('study_duration');
+                    var_dump($province_hospital->id);
+                    foreach ($hospital_doctors as $hospital_doctor){
+                        if($hospital_doctor){
+                            // 等级统计
+                            if($hospital_doctor->rank > 0){
+                                $doctor_level[$hospital_doctor->rank]++;
+                            }
+                            // 时长统计
+                            foreach ($course_lists as $course_list){
+                                $study = StudyLog::whereIn('course_id', $course_list['course_ids'])
+                                    ->where(['site_id' => $this->site_id, 'doctor_id' => $hospital_doctor->id])
+                                    ->get();
+                                if($course_list['id'] == $this->public_class_id){
+                                    $study_duration['public'] += $study->sum('study_duration');
+                                }else if($course_list['id'] == $this->answer_class_id){
+                                    $study_duration['answer'] += $study->sum('study_duration');
+                                }
+                                $click_num += $study->count();
+                            }
                         }
-
-                        $click_num += $study->count();
-
                     }
                 }
             }
@@ -99,9 +109,9 @@ class ReportController extends Controller
             $data[$key + 1][] = $doctor_level[1];
             $data[$key + 1][] = $doctor_level[2];
             $data[$key + 1][] = $doctor_level[3];
-            dd($data);
+//            dd($data);
         }
-dd($data);
+//dd($data);
         Excel::create(date('Y-m-d') . '导出报表',function($excel) use ($data){
             $excel->sheet('Sheet1', function($sheet) use ($data){
                 $sheet->rows($data);
@@ -111,11 +121,75 @@ dd($data);
     }
 
     public function export2ExcelDoctorInfo(){
-        $sign_info = KZKTClass::where(['site_id' => $this->site_id, 'status' => 1])->paginate($this->export_sign_info);
-        foreach ($sign_info as $val){
-            dd($val);
+        set_time_limit(0);
+        $data = [[
+            '手机号', '学员姓名', '大区', '省', '市', '县', '医院', '医院等级', '科室', '职称','邮箱',
+            '是否电话外呼', '大区经理', '代表', '代表手机号',
+            '总时长', '答疑课时长', '理论课时长', '点击总数', '答疑课点击数','理论课点击数',
+        ]];
+        $course_lists = $this->course_category();
+
+        $sign_info = KZKTClass::where(['site_id' => $this->site_id, 'status' => 1])
+            ->paginate($this->export_sign_info);
+
+        foreach ($sign_info as $key => $val){
+            $data[$key + 1][] = $val->doctor->phone ?? '';
+            $data[$key + 1][] = $val->doctor->name ?? '';
+            $data[$key + 1][] = $val->volunteer->represent->belong_area ?? '';
+            $data[$key + 1][] = $val->doctor->hospital->province ?? '';
+            $data[$key + 1][] = $val->doctor->hospital->city ?? '';
+            $data[$key + 1][] = $val->doctor->hospital->country ?? '';
+            $data[$key + 1][] = $val->doctor->hospital->hospital ?? '';
+            $data[$key + 1][] = $val->doctor->hospital->hospital_level ?? '';
+            $data[$key + 1][] = $val->doctor->office ?? '';
+            $data[$key + 1][] = $val->doctor->title ?? '';
+            $data[$key + 1][] = $val->doctor->email;
+            $data[$key + 1][] = $val->style == 'phone' ? '是' : '否';
+            $data[$key + 1][] = $val->volunteer->represent->belong_dbm ?? '';
+            $data[$key + 1][] = $val->volunteer->name ?? '';
+            $data[$key + 1][] = $val->volunteer->phone ?? '';
+
+            // 学习时长
+            $study_duration = [
+                'total' => 0,
+                'public' => 0,
+                'answer' => 0,
+            ];
+            // 点击量
+            $click_num = [
+                'total' => 0,
+                'public' => 0,
+                'answer' => 0,
+            ];
+            // 时长统计
+            foreach ($course_lists as $course_list){
+                $study = StudyLog::whereIn('course_id', $course_list['course_ids'])
+                    ->where(['site_id' => $this->site_id, 'doctor_id' => $val->doctor->id])
+                    ->get();
+                if($course_list['id'] == $this->public_class_id){
+                    $study_duration['public'] += $study->sum('study_duration');
+                    $click_num['public'] = $study->count();
+                }else if($course_list['id'] == $this->answer_class_id){
+                    $study_duration['answer'] += $study->sum('study_duration');
+                    $click_num['answer'] = $study->count();
+                }
+            }
+
+            $study_duration['total'] = $study_duration['public'] + $study_duration['answer'];
+            $data[$key + 1][] = $study_duration['total'];
+            $data[$key + 1][] = $study_duration['answer'];
+            $data[$key + 1][] = $study_duration['public'];
+            $click_num['total'] = $click_num['public'] + $click_num['answer'];
+            $data[$key + 1][] = $click_num['total'];
+            $data[$key + 1][] = $click_num['answer'];
+            $data[$key + 1][] = $click_num['public'];
         }
-        dd($sign_info);
+//        dd($data);
+        Excel::create(date('Y-m-d') . '导出报表',function($excel) use ($data){
+            $excel->sheet('Sheet1', function($sheet) use ($data){
+                $sheet->rows($data);
+            });
+        })->export('xlsx');
     }
 
 }
