87394e8070da699e08bc0a20da2fc7e39d40e7c1
zhaiyu <zhaiyu_hb@hotmail.com>
2017-05-15 15:51:47 +0800

    视频播放处理无效果

--- public/airclass/js/jquery.common.js
+++ public/airclass/js/jquery.common.js
@@ -255,14 +255,4 @@ $(function () {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
-
-    $('.modal').on('show.bs.modal', function (e) {
-        $('#id_video_container').find('object').attr('hidden','true');
-        console.log('show');
-    });
-
-    $('.modal').on('hide.bs.modal', function (e) {
-        $('#id_video_container').find('object').removeAttr('hidden');
-        console.log('hide');
-    });
 });
