3f0042962968cdd5140a88edb4b4ff61e91d49d3
junfeng0202 <1628439099@qq.com>
2017-12-29 14:47:54 +0800

    课程权限开放

--- resources/views/modules/airclass/home/public_class.blade.php
+++ resources/views/modules/airclass/home/public_class.blade.php
@@ -25,7 +25,7 @@
                                 <p class="introduction">{{ str_limit($val->comment, $limit = 100, $end = '...') }}</p>
                                 <p class="price_and_persons">
                                     {{--<span class="price">&yen;198.00</span>--}}
-                                    @if($val->course_type == 2)<span class="class_list_lock_icon pull-right"></span>@endif
+{{--                                    @if($val->course_type == 2)<span class="class_list_lock_icon pull-right"></span>@endif--}}
                                     <span class="persons pull-right">{{ $val->play_count }}人在学</span>
                                 </p>
                             </div>
